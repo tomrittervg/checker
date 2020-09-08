4f91d11b54a5008af7a76043035d0acbbff2392c
Tom Ritter <tom@ritter.vg>
2020-09-07 23:56:09 -0400

    env python -> python3

--- jobs/JobSpawner.py
+++ jobs/JobSpawner.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 
 from builtins import object
 class JobSpawner(object):
--- statustracker.py
+++ statustracker.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 
 from builtins import object
 import time
