f75bc4c6499ffb1594356bacb38567a0c4ce882f
Tom Ritter <tom@ritter.vg>
2024-11-19 11:28:49 -0500

    black files

--- jobs/TCPServerChecker.py
+++ jobs/TCPServerChecker.py
@@ -8,17 +8,29 @@ import logging
 from . import JobBase
 from . import JobSpawner
 
+
 class TCPServerChecker(JobSpawner.JobSpawner):
-    servers = [ 
-                #("example.com", 53, "example.com:tcpdns", JobBase.JobFrequency.MINUTE, JobBase.JobFailureNotificationFrequency.EVERYTIME, JobBase.JobFailureCountMinimumBeforeNotification.ONE)
-              ]
+    servers = [
+        # ("example.com", 53, "example.com:tcpdns", JobBase.JobFrequency.MINUTE, JobBase.JobFailureNotificationFrequency.EVERYTIME, JobBase.JobFailureCountMinimumBeforeNotification.ONE)
+    ]
 
     class ServerChecker(JobBase.JobBase):
-        def __init__(self, config, ip, port, friendlyName, frequency, failureNotificationFrequency, failuresBeforeNotification):
+        def __init__(
+            self,
+            config,
+            ip,
+            port,
+            friendlyName,
+            frequency,
+            failureNotificationFrequency,
+            failuresBeforeNotification,
+        ):
             self.config = config
             self.ip = ip
             self.port = port
-            self.friendlyName = friendlyName + "(" + self.ip + ":" + str(self.port) + ")"
+            self.friendlyName = (
+                friendlyName + "(" + self.ip + ":" + str(self.port) + ")"
+            )
             self.frequency = frequency
             self.failureNotificationFrequency = failureNotificationFrequency
             self.failuresBeforeNotification = failuresBeforeNotification
@@ -26,12 +38,16 @@ class TCPServerChecker(JobSpawner.JobSpawner):
 
         def getName(self):
             return str(self.__class__) + " for " + self.friendlyName
+
         def executeEvery(self):
             return self.frequency
+
         def notifyOnFailureEvery(self):
             return self.failureNotificationFrequency
+
         def numberFailuresBeforeNotification(self):
             return self.failuresBeforeNotification
+
         def execute(self):
             try:
                 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -43,13 +59,13 @@ class TCPServerChecker(JobSpawner.JobSpawner):
                 self.failuremsg = "Could not hit server " + self.friendlyName
                 logging.warn(self.failuremsg)
                 return False
+
         def onFailure(self):
             return self.sendEmail(self.failuremsg, "")
+
         def onStateChangeSuccess(self):
             return self.sendEmail("Successfully hit " + self.friendlyName, "")
 
     def get_sub_jobs(self, config):
         for s in self.servers:
             yield self.ServerChecker(config, s[0], s[1], s[2], s[3], s[4], s[5])
-
-            
--- jobs/TLSCertExpiration.py
+++ jobs/TLSCertExpiration.py
@@ -11,13 +11,26 @@ import datetime
 from . import JobBase
 from . import JobSpawner
 
+
 class TLSCertExpiration(JobSpawner.JobSpawner):
-    servers = [ 
-            	
-              ]
+    servers = [
+        (
+            "ritter.vg:443",
+            JobBase.JobFrequency.HOUR,
+            JobBase.JobFailureNotificationFrequency.EVERYDAY,
+            JobBase.JobFailureCountMinimumBeforeNotification.ONE,
+        ),
+    ]
 
     class CertChecker(JobBase.JobBase):
-        def __init__(self, config, url, frequency, failureNotificationFrequency, failuresBeforeNotification):
+        def __init__(
+            self,
+            config,
+            url,
+            frequency,
+            failureNotificationFrequency,
+            failuresBeforeNotification,
+        ):
             self.config = config
             self.url = url
             self.frequency = frequency
@@ -27,36 +40,55 @@ class TLSCertExpiration(JobSpawner.JobSpawner):
 
         def getName(self):
             return str(self.__class__) + " for " + self.url
+
         def executeEvery(self):
             return self.frequency
+
         def notifyOnFailureEvery(self):
             return self.failureNotificationFrequency
+
         def numberFailuresBeforeNotification(self):
             return self.failuresBeforeNotification
+
         def execute(self):
             try:
                 context = ssl._create_unverified_context()
                 c = http.client.HTTPSConnection(self.url, context=context)
                 c.request("GET", "/")
                 asn1 = c.sock.getpeercert(True)
-                x509 = OpenSSL.crypto.load_certificate(OpenSSL.crypto.FILETYPE_ASN1, asn1)
-                na = time.mktime(time.strptime(x509.get_notAfter()[:-1].decode("utf-8"), '%Y%m%d%H%M%S'))
+                x509 = OpenSSL.crypto.load_certificate(
+                    OpenSSL.crypto.FILETYPE_ASN1, asn1
+                )
+                na = time.mktime(
+                    time.strptime(
+                        x509.get_notAfter()[:-1].decode("utf-8"), "%Y%m%d%H%M%S"
+                    )
+                )
                 now = time.time()
                 delta = datetime.timedelta(seconds=(na - now))
                 if delta < datetime.timedelta(days=30):
-                    self.failuremsg = "Server Certificate for " + self.url + " expires in " + str(delta.days) + " days"
+                    self.failuremsg = (
+                        "Server Certificate for "
+                        + self.url
+                        + " expires in "
+                        + str(delta.days)
+                        + " days"
+                    )
                     return False
                 return True
             except Exception as e:
-                self.failuremsg = "Could not get server certificate " + self.url + "\n" + str(e)
+                self.failuremsg = (
+                    "Could not get server certificate " + self.url + "\n" + str(e)
+                )
                 logging.warn(self.failuremsg)
                 return False
+
         def onFailure(self):
             return self.sendEmail(self.failuremsg, "")
+
         def onStateChangeSuccess(self):
             return self.sendEmail("Successfully hit " + self.url, "")
 
     def get_sub_jobs(self, config):
         for s in self.servers:
             yield self.CertChecker(config, s[0], s[1], s[2], s[3])
-
